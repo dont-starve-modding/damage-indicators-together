ec92ddbe027176c153c11125f8af1070fa45770b
aehrraid <it@simonkoehler.eu>
2017-06-06 19:45:13 +0200

    add mod files

---
+++ modinfo.lua
@@ -0,0 +1,70 @@
+name = "Damage Indicators"
+description = "Baaaaam Baam. Take that, nasty brute of hell!"
+author = "s1m13, ported by DarkXero"
+version = "0.69"
+forumthread = "/topic/30774-mod-idea-damage-indicator/"
+api_version = 10
+dont_starve_compatible = false
+reign_of_giants_compatible = false
+dst_compatible = true
+icon_atlas = "damageindicators.xml"
+icon = "damageindicators.tex"
+all_clients_require_mod = true
+client_only_mod = false
+server_filter_tags = { "damage indicators", "floating numbers" }
+
+
+configuration_options =
+  {
+    {
+      name = "dmg_only",
+      label = "Show Damage Only",
+      options = {
+        {description = "On", data = "on"},
+        {description = "Off", data = "off"},
+      },
+
+      default = "on",
+
+    },
+
+    {
+      name = "display_mode",
+      label = "Display Mode",
+      options = {
+        {description = "Bouncy", data = "bouncy"},
+        {description = "Waving", data = "waving"},
+        {description = "Straight", data = "straight"},
+      },
+
+      default = "waving",
+
+    },
+
+
+    {
+      name = "amount_of_numbers",
+      label = "Amount of Numbers",
+      options = {
+        {description = "Low", data = "low"},
+        {description = "Default", data = "default"},
+        {description = "High", data = "high"},
+      },
+
+      default = "default",
+
+    },
+
+    {
+      name = "show_decimal_points",
+      label = "Show Decimal Points for",
+      options = {
+        {description = "No Numbers", data = "no"},
+        {description = "Low Numbers", data = "low"},
+        {description = "All Numbers", data = "all"},
+      },
+
+      default = "no",
+
+    },
+  }
---
+++ modmain.lua
@@ -0,0 +1,73 @@
+PrefabFiles = {
+	"dmgind",
+}
+
+
+----------------------------------------------------
+----------------- CONFIGURATION --------------------
+
+TUNING.SHOW_DAMAGE_ONLY = GetModConfigData("dmg_only") == "on"		-- set to true, if you want to see damage only (no healing)
+
+TUNING.SHOW_NUMBERS_THRESHOLD = 0.1
+if GetModConfigData("amount_of_numbers") == "high" then
+	TUNING.SHOW_NUMBERS_THRESHOLD = 0.001
+end
+if GetModConfigData("amount_of_numbers") == "low" then
+	TUNING.SHOW_NUMBERS_THRESHOLD = 0.99
+end
+
+TUNING.SHOW_DECIMAL_POINTS = GetModConfigData("show_decimal_points")
+
+TUNING.DISPLAY_MODE = GetModConfigData("display_mode")
+
+----------------------------------------------------
+
+
+TUNING.HEALTH_LOSE_COLOR = {
+	r = 0.7,
+	g = 0,
+	b = 0
+}
+TUNING.HEALTH_GAIN_COLOR = {
+	r = 0,
+	g = 0.7,
+	b = 0
+}
+
+
+TUNING.LABEL_FONT_SIZE = 70
+
+TUNING.LABEL_Y_START = 4
+
+TUNING.LABEL_TIME = 1.0
+
+TUNING.LABEL_TIME_DELTA = 0.01
+
+TUNING.GRAVITY = 0.1
+TUNING.FRICTION_PRESERVE = 0.9
+
+TUNING.LIFT_ACC = 0.003
+TUNING.SIDE_WAVE_RND = 0.15
+TUNING.LABEL_Y_START_VELO = 0.05
+
+TUNING.LABEL_MIN_AMPLITUDE_X = 0.8
+TUNING.LABEL_MAX_AMPLITUDE_X = 1.6
+
+
+AddComponentPostInit("health", function(Health, inst)
+	inst:ListenForEvent("healthdelta", function(inst, data)
+		if inst.components.health then
+			local amount = (data.newpercent - data.oldpercent) * inst.components.health.maxhealth
+			if math.abs(amount) > TUNING.SHOW_NUMBERS_THRESHOLD then
+				if not (TUNING.SHOW_DAMAGE_ONLY and amount > 0) then
+					local CDI = GLOBAL.SpawnPrefab("dmgind")
+					CDI.Transform:SetPosition(inst.Transform:GetWorldPosition())
+					CDI.isheal:set_local(false)
+					CDI.isheal:set(amount > 0)
+					CDI.indicator:set_local(0)
+					CDI.indicator:set(math.abs(amount))
+				end
+			end
+		end
+	end)
+end)
