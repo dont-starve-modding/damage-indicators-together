f12241db95d8b679c578eac0e4aff7fafc727d36
Aehrraid <Aehrraid@users.noreply.github.com>
2019-05-25 09:35:25 +0200

    make build script more verbose

--- build.sh
+++ build.sh
@@ -1,9 +1,14 @@
+echo "removing artifacts..."
 rm -r build/
 
 mkdir -p build/scripts
+
+echo "copying..."
 cp -r scripts/* build/scripts/
 cp CONTRIBUTORS build/
 cp damageindicators.* build/
 cp LICENSE build/
 cp *.lua build/
 cp README* build/
+
+echo "Finished."
